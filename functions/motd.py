6c91ff8623fff1ccdaea83ffd454844bf8519fc3
Jaysonc04 <43862615+Jaysonc04@users.noreply.github.com>
2019-03-24 13:03:29 -0400

    Update motd.py

--- functions/motd.py
+++ functions/motd.py
@@ -6,4 +6,11 @@ import error
 import files
 
 def view(guild, server=True):
-    f = files.read()
+    f = files.read("../Bot-Storage/" + guild + ".json", True)
+ 
+    if f == "FileNotFoundError":
+        return error.gen("You have not setup your server yet!")
+    elif f == "JSONDecodeError":
+        return error.gen("Your server's file seems to be corrupted, please contact us or reset your server. \n\n!contact \n!reset")
+    
+    r = requests
